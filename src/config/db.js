f30eac76f4b5322601b5df0b4e470e5fed3c3c99
Pythoncraver1 <111011373+BossMM12@users.noreply.github.com>
2024-12-10 17:20:59 +0100

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const connect = await mongoose.connect(process.env.MONGO_URI, { 
+        await mongoose.connect(process.env.MONGO_URI,{ 
             useNewUrlParser: true, 
             useUnifiedTopology: true,
-            userCreateIndex: true, 
-        });
-        console.log('Database connected successfully: ${connect.connection.host}');
+         });
+        console.log('Database connected successfully');
     } catch (error) {
         console.error('Database connection failed', error);
         process.exit(1);
